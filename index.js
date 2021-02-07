f67a6f68efa34b7a48ba6405edf5d25d0c77d132
Ojashri Basnyat <basnyat.ojashri@gmail.com>
2021-02-07 14:20:54 -0500

    fixed a small in the index code

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Ext packages
 const inquirer = require('inquirer');
-const fs = require(fs);
+const fs = require('fs');
 const util = require('util');
 
 // int modules
@@ -13,7 +13,7 @@ const questions = [
         type: 'input',
         message: "What is your GitHub username? (No @ needed)",
         name: 'username',
-        default: 'connietran-dev',
+        default: 'Ojashri-Basnyat',
         validate: function (answer) {
             if (answer.length < 1) {
                 return console.log("A valid GitHub username is required.");
